e5bfa2f690bd6b206be1f2bcd8b71de4534295f6
rubanik <anton.rubanik@gmail.com>
2026-05-15 22:07:21 +0300

    ux improvements

--- frontend/src/components/auth/AuthProvider.jsx
+++ frontend/src/components/auth/AuthProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, createContext } from 'react';
+import React, { useState, useEffect, useCallback, createContext, useContext } from 'react';
 import {
   Dialog,
   DialogTitle,
